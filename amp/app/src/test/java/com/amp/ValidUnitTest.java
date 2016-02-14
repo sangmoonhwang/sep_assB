56284455a4c2a5ac7233ca4195beecd57852b67f
sangmoonhwang <ghkdtkdans@gmail.com>
2016-02-14 03:13:50 -0500

    Valid Canada Meter or Postal Indicia

--- amp/app/src/test/java/com/amp/ValidUnitTest.java
+++ amp/app/src/test/java/com/amp/ValidUnitTest.java
@@ -38,4 +38,48 @@ public class ValidUnitTest {
         for (int weight = 201; weight <= 300; weight++)
             assertEquals(RateCalculator.getRate("canada", "stamp", weight), 4.10, 0.001);
     }
+
+    @Test
+    public void canadaMeterStandards() {
+        for (int weight = 0; weight <= 30; weight++)
+            assertEquals(RateCalculator.getRate("canada", "meter", weight), 0.80, 0.001);
+        for (int weight = 31; weight <= 50; weight++)
+            assertEquals(RateCalculator.getRate("canada", "meter", weight), 1.19, 0.001);
+    }
+
+    @Test
+    public void canadaMeterNonStandards() {
+        for (int weight = 401; weight <= 500; weight++)
+            assertEquals(RateCalculator.getRate("canada", "meter", weight), 4.74, 0.001);
+        for (int weight = 301; weight <= 400; weight++)
+            assertEquals(RateCalculator.getRate("canada", "meter", weight), 4.42, 0.001);
+        for (int weight = 51; weight <= 100; weight++)
+            assertEquals(RateCalculator.getRate("canada", "meter", weight), 1.71, 0.001);
+        for (int weight = 101; weight <= 200; weight++)
+            assertEquals(RateCalculator.getRate("canada", "meter", weight), 2.77, 0.001);
+        for (int weight = 201; weight <= 300; weight++)
+            assertEquals(RateCalculator.getRate("canada", "meter", weight), 3.89, 0.001);
+    }
+
+    @Test
+    public void canadaPostalIndiciaStandards() {
+        for (int weight = 0; weight <= 30; weight++)
+            assertEquals(RateCalculator.getRate("canada", "postal indicia", weight), 0.80, 0.001);
+        for (int weight = 31; weight <= 50; weight++)
+            assertEquals(RateCalculator.getRate("canada", "postal indicia", weight), 1.19, 0.001);
+    }
+
+    @Test
+    public void canadaPostalIndiciaNonStandards() {
+        for (int weight = 401; weight <= 500; weight++)
+            assertEquals(RateCalculator.getRate("canada", "postal indicia", weight), 4.74, 0.001);
+        for (int weight = 301; weight <= 400; weight++)
+            assertEquals(RateCalculator.getRate("canada", "postal indicia", weight), 4.42, 0.001);
+        for (int weight = 51; weight <= 100; weight++)
+            assertEquals(RateCalculator.getRate("canada", "postal indicia", weight), 1.71, 0.001);
+        for (int weight = 101; weight <= 200; weight++)
+            assertEquals(RateCalculator.getRate("canada", "postal indicia", weight), 2.77, 0.001);
+        for (int weight = 201; weight <= 300; weight++)
+            assertEquals(RateCalculator.getRate("canada", "postal indicia", weight), 3.89, 0.001);
+    }
 }
