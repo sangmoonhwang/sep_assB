47f0738aeb7280d8ff043f95af15762626c68204
sangmoonhwang <ghkdtkdans@gmail.com>
2016-02-14 03:10:31 -0500

    Valid Canada Non_Standard Stamps

--- amp/app/src/test/java/com/amp/ValidUnitTest.java
+++ amp/app/src/test/java/com/amp/ValidUnitTest.java
@@ -11,16 +11,31 @@ public class ValidUnitTest {
 
     @Test
     public void canadaSingleStampsStandards() throws Exception {
-        for(int weight = 0; weight <= 30; weight++)
+        for (int weight = 0; weight <= 30; weight++)
             assertEquals(RateCalculator.getRate("canada", "stamp", weight), 1.00, 0.001);
-        for(int weight = 31; weight <= 50; weight++)
+        for (int weight = 31; weight <= 50; weight++)
             assertEquals(RateCalculator.getRate("canada", "stamp", weight), 1.20, 0.001);
     }
+
     @Test
     public void canadaSpecialStamps() throws Exception {
-        for(int weight = 0; weight <= 30; weight++)
+        for (int weight = 0; weight <= 30; weight++)
             assertEquals(RateCalculator.getRate("canada", "stamp special", weight), 0.85, 0.001);
-        for(int weight = 31; weight <= 50; weight++)
+        for (int weight = 31; weight <= 50; weight++)
             assertEquals(RateCalculator.getRate("canada", "stamp special", weight), 1.20, 0.001);
+    }
+
+    @Test
+    public void canadaStampsNonStandards() {
+        for (int weight = 401; weight <= 500; weight++)
+            assertEquals(RateCalculator.getRate("canada", "stamp", weight), 5.05, 0.001);
+        for (int weight = 301; weight <= 400; weight++)
+            assertEquals(RateCalculator.getRate("canada", "stamp", weight), 4.70, 0.001);
+        for (int weight = 51; weight <= 100; weight++)
+            assertEquals(RateCalculator.getRate("canada", "stamp", weight), 1.80, 0.001);
+        for (int weight = 101; weight <= 200; weight++)
+            assertEquals(RateCalculator.getRate("canada", "stamp", weight), 2.95, 0.001);
+        for (int weight = 201; weight <= 300; weight++)
+            assertEquals(RateCalculator.getRate("canada", "stamp", weight), 4.10, 0.001);
     }
 }
