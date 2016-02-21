bf4ca19712218d612ab070d1d00834ee0d623b55
sfostine <samuel.fostine@mail.mcgill.ca>
2016-02-21 01:32:33 -0500

    UI, CANADA and USA done

--- amp/app/src/main/java/com/amp/RateCalculator.java
+++ amp/app/src/main/java/com/amp/RateCalculator.java
@@ -28,67 +28,136 @@ public class RateCalculator {
 
 
     // compute the rate
-    public double getRate(String country_destination, String type, String item, double weight) {
+    public double getRate(String country_destination, String type, String item, double weight)
+            throws InvalidParameterException {
 
         // weight must be greater than 0 or less or equal to 500
         // otherwise return an exception
+        if (weight > 500 || weight < 0) {
+            throw new InvalidParameterException(WEIGHT_EXCEPTION);
+        }
 
+        // FOR CANADA
         if (country_destination.equals(CANADA)) {
-            if (type.equals(STANDARD)) {
-                    switch (item) {
-                        case STAMP_BCP:
-                            // Canada, standard, Stamps in booklets/coils/panes, Up to 30 g
-                            if (weight > 0 && weight <= 30) return 0.85;
-                            // over 30, up to 50
-                            else if(weight > 30 && weight <= 50) return 1.20;
-                            // case of meter or postal Indicia
-                            else if(weight > 50)
-                                throw new InvalidParameterException(WEIGHT_EXCEPTION);
-                        case METER_OR_POSTALINDICIA:
-                            // up to 30 g
-                            if (weight > 0 && weight <= 30) return 0.80;
+            return rateForCanada(type, item, weight);
+        }
 
-                            // over 30 up to 50
-                            else if(weight > 30 && weight <= 50) return 1.19;
-                            else if(weight > 50)
-                                throw new InvalidParameterException(WEIGHT_EXCEPTION);
-                            // case sinple stamp
-                        case SINGLE_STAMP:
-                            // up to 30
-                            if (weight > 0 && weight <= 30) return 1.00;
-                            //over 30, up to 50
-                            else if(weight > 30 && weight <= 50) return 1.20;
-
-                    }
-            }
-
-            // if Other (non standard, oversize)
-            else{
-
-                switch (item){
-                    // stamp
-                    case STAMP:
-                        // greater than 50, less or equal to 100
-                        if (weight > 50 && weight <= 100) return 2.95;
-                            // greater than 100, less or equal to 200
-                        else if (weight > 100 && weight <= 200) return 5.15;
-                            // greater than 200, up to 500
-                        else if (weight > 200 && weight <= 500) return 10.30;
-                        //case meter or postal indicia
-
-                    case METER_OR_POSTALINDICIA:
-                        // over 50 up to 100
-                        if (weight > 50 && weight <= 100) return 2.68;
-                            // greater than 100, less or equal to 200
-                        else if (weight > 100 && weight <= 200) return 4.85;
-                            // greater than 200, up to 500
-                        else if (weight > 200 && weight <= 500) return 9.69;
-                }
-
-            }
+        // FOR USA
+        if (country_destination.equals(USA))
+        {
+            return rateForUSA(type, item, weight);
         }
         return 0.0;
     }
+
+    private double rateForUSA(String type, String item, double weight)
+    {
+        if (type.equals(LETTER_POST)) {
+            switch (item) {
+                case STAMP:
+                    // USA, standard, Stamps, Up to 30 g
+                    if (weight > 0 && weight <= 30) return 1.20;
+
+                        // over 30, up to 50
+                    else if (weight > 30 && weight <= 50) return 1.80;
+                case METER_OR_POSTALINDICIA:
+                    // USA, standard, meter or postal Indicia, Up to 30 g
+                    if (weight > 0 && weight <= 30) return 1.19;
+                        // over 30, up to 50
+                    else if (weight > 30 && weight <= 50) return 1.72;
+            }
+        }
+        else if(type.equals(OTHER)) {
+            switch (item){
+                case STAMP:
+                    // USA, OTHER, Stamps, over 50 Up to 100 g
+                    if (weight > 50 && weight <= 100) return 2.95;
+                        // greater than 100, less or equal to 200
+                    else if (weight > 100 && weight <= 200) return 5.15;
+                        // greater than 200, less or equal to 500
+                    else if (weight > 200 && weight <= 500) return 10.30;
+                    break;
+                case METER_OR_POSTALINDICIA:
+                    // USA, OTHER, METER or POSTAL INDICIA, over 50 Up to 100 g
+                    if (weight > 50 && weight <= 100) return 2.68;
+                        // greater than 100, less or equal to 200
+                    else if (weight > 100 && weight <= 200) return 4.85;
+                        // greater than 200, less or equal to 500
+                    else if (weight > 200 && weight <= 500) return 9.69;
+                    break;
+
+            }
+        }
+        throw new InvalidParameterException(WEIGHT_EXCEPTION);
+
+    }
+
+
+
+    private double rateForCanada(String type, String item, double weight) {
+        if (type.equals(STANDARD)) {
+            switch (item) {
+                case STAMP_BCP:
+                    // Canada, standard, Stamps in booklets/coils/panes, Up to 30 g
+                    if (weight > 0 && weight <= 30) return 0.85;
+                        // over 30, up to 50
+                    else if(weight > 30 && weight <= 50) return 1.20;
+                        // case of meter or postal Indicia
+                case METER_OR_POSTALINDICIA:
+                    // up to 30 g
+                    if (weight > 0 && weight <= 30) return 0.80;
+
+                        // over 30 up to 50
+                    else if(weight > 30 && weight <= 50) return 1.19;
+                   // case sinple stamp
+                case SINGLE_STAMP:
+                    // up to 30
+                    if (weight > 0 && weight <= 30) return 1.00;
+                        //over 30, up to 50
+                    else if(weight > 30 && weight <= 50) return 1.20;
+            }
+        }
+        // if Other (non standard, oversize)
+        else{
+            switch (item){
+                // stamp
+                case STAMP:
+                    //greater than 50, less or equal to 100
+                    if (weight > 50 && weight <= 100) return 1.80;
+                    // greater than 100, less or equal to 200
+                    else if (weight > 100 && weight <= 200) return 2.95;
+                        // greater than 200, less or equal to 300
+                    else if (weight > 200 && weight <= 300) return 4.10;
+                        // greater than 300, less or equal to 400
+                    else if (weight > 300 && weight <= 400) return 4.70;
+                        // greater than 400, less or equal to 500
+                    else if (weight > 400 && weight <= 500) return 5.05;
+
+                    //case meter or postal indicia
+                    break;
+
+                case METER_OR_POSTALINDICIA:
+                    // over 50 up to 100
+                    if (weight > 50 && weight <= 100) return 1.71;
+                        // greater than 100, less or equal to 200
+                    else if (weight > 100 && weight <= 200) return 2.77;
+                        // greater than 200, less or equal to 300
+                    else if (weight > 200 && weight <= 300) return 3.89;
+                        // greater than 300, less or equal to 400
+                    else if (weight > 300 && weight <= 400) return 4.42;
+                        // greater than 400, less or equal to 500
+                    else if (weight > 400 && weight <= 500) return 4.74;
+
+                    break;
+            }
+
+        }
+        // if none of the above wight, through an exception
+        //return 0.0;
+        throw new InvalidParameterException(WEIGHT_EXCEPTION);
+    }
+
+
 
 }
 
--- amp/app/src/test/java/com/amp/RateCalculatorTest.java
+++ amp/app/src/test/java/com/amp/RateCalculatorTest.java
@@ -38,90 +38,264 @@ public class RateCalculatorTest {
     @Test
     public void canada_Standard_stampBCP_UpTo30g()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
-                RateCalculator.STAMP_BCP, 30), 0.85, 0.001);
+        for(int i = 1; i <=30; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
+                    RateCalculator.STAMP_BCP, i), 0.85, 0.001);
+        }
     }
 
     @Test
     public void canada_Standard_stampBCP_Over30_UpTo50g()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
-                RateCalculator.STAMP_BCP, 31), 1.20, 0.001);
+        for(int i = 31; i <=50; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
+                    RateCalculator.STAMP_BCP, i), 1.20, 0.001);
+        }
     }
 
     @Test
     public void canada_Standard_METER_OR_POSTALINDICIA_UpTo30g()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
-                RateCalculator.METER_OR_POSTALINDICIA, 20), 0.80, 0.001);
+        for(int i = 1; i <=30; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 0.80, 0.001);
+        }
     }
 
     @Test
     public void canada_Standard_METER_OR_POSTALINDICIA_Over30_UpTo50g()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
-                RateCalculator.METER_OR_POSTALINDICIA, 40), 1.19, 0.001);
+        for(int i = 31; i <=50; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 1.19, 0.001);
+        }
     }
 
 
     @Test
     public void canada_Standard_SINGLESTAMP_UpTo30g()
     {
+        for(int i = 1; i <=30; i++){
         assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
-                RateCalculator.SINGLE_STAMP, 10), 1.00, 0.001);
+                RateCalculator.SINGLE_STAMP, i), 1.00, 0.001);
+        }
     }
 
     @Test
     public void canada_Standard_SINGLESTAMP_Over30_UpTo50g()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
-                RateCalculator.SINGLE_STAMP, 50), 1.20, 0.001);
+        for(int i = 31; i <=50; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.STANDARD,
+                    RateCalculator.SINGLE_STAMP, i), 1.20, 0.001);
+        }
     }
 
     @Test
     public void canada_OTHER_STAMP_OVER50_UpTo100g()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
-                RateCalculator.STAMP, 51), 2.95, 0.001);
+        for(int i = 51; i <=100; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.STAMP, i), 1.80, 0.001);
+        }
     }
 
     @Test
     public void canada_OTHER_STAMP_OVER100_UpTo200g()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
-                RateCalculator.STAMP, 200), 5.15, 0.001);
+        for(int i = 101; i <=200; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.STAMP, i), 2.95, 0.001);
+        }
+    }
+
+    @Test
+    public void canada_OTHER_STAMP_OVER200_UpTo300g()
+    {
+        for(int i = 201; i <=300; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.STAMP, i), 4.10, 0.001);
+        }
+    }
+
+    @Test
+    public void canada_OTHER_STAMP_OVER300_UpTo400g()
+    {
+        for(int i = 301; i <=400; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.STAMP, i), 4.70, 0.001);
+        }
+    }
+
+    @Test
+    public void canada_OTHER_STAMP_OVER400_UpTo500g()
+    {
+        for(int i = 401; i <=500; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.STAMP, i), 5.05, 0.001);
+        }
+    }
+
+    @Test
+    public void canada_OTHER_METER_OR_POSTALINDICIA_OVER50_UpTo100g()
+    {
+        for(int i = 51; i <=100; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 1.71, 0.001);
+        }
+    }
+
+    @Test
+    public void canada_OTHER_METER_OR_POSTALINDICIA_OVER100_UpTo200g()
+    {
+        for(int i = 101; i <=200; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 2.77, 0.001);
+        }
+    }
+
+    @Test
+    public void canada_OTHER_METER_OR_POSTALINDICIA_OVER200_UpTo300g()
+    {
+        for(int i = 201; i <=300; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 3.89, 0.001);
+        }
+    }
+
+    @Test
+    public void canada_OTHER_METER_OR_POSTALINDICIA_OVER300_UpTo400g()
+    {
+        for(int i = 301; i <=400; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 4.42, 0.001);
+        }
+    }
+
+    @Test
+    public void canada_OTHER_METER_OR_POSTALINDICIA_OVER400_UpTo500g()
+    {
+        for(int i = 401; i <=500; i++) {
+            assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 4.74, 0.001);
+        }
+    }
+
+    @Test
+    public void usa_LetterPost_stamp_UpTo30g()
+    {
+        for(int i = 1; i <=30; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.LETTER_POST,
+                    RateCalculator.STAMP, i), 1.20, 0.001);
+        }
+    }
+
+    @Test
+    public void usa_LetterPost_stamp_Over30_UpTo50g()
+    {
+        for(int i = 31; i <=50; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.LETTER_POST,
+                    RateCalculator.STAMP, i), 1.80, 0.001);
+        }
     }
 
 
     @Test
-    public void canada_OTHER_STAMP_OVER200_UpTo500g()
+    public void usa_LetterPost_stamp_Over50_INVALID()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
-                RateCalculator.STAMP, 300), 10.30, 0.001);
+        try {
+            rate.getRate(RateCalculator.USA, RateCalculator.LETTER_POST,
+                    RateCalculator.STAMP, 51);
+        } catch (InvalidParameterException e) {
+            assertEquals(e.getMessage(), RateCalculator.WEIGHT_EXCEPTION);
+        }
     }
 
     @Test
-    public void canada_OTHER_METER_OR_POSTALINDICIA_Over50_UpTo100g()
+    public void usa_LetterPost_METER_OR_POSTALINDICIA_UpTo30()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
-                RateCalculator.METER_OR_POSTALINDICIA, 51), 2.68, 0.001);
+        for(int i = 1; i <=30; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.LETTER_POST,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 1.19, 0.001);
+        }
     }
 
     @Test
-    public void canada_OTHER_METER_OR_POSTALINDICIA_Over100_UpTo200g()
+    public void usa_LetterPost_METER_OR_POSTALINDICIA_Over30_UpTo50()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
-                RateCalculator.METER_OR_POSTALINDICIA, 101), 4.85, 0.001);
+        for(int i = 31; i <=50; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.LETTER_POST,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 1.72, 0.001);
+        }
+    }
+
+    public void usa_LetterPost_METER_OR_POSTALINDICIA_Over50_INVALID()
+    {
+        try {
+            rate.getRate(RateCalculator.USA, RateCalculator.LETTER_POST,
+                    RateCalculator.METER_OR_POSTALINDICIA, 51);
+        } catch (InvalidParameterException e) {
+            assertEquals(e.getMessage(), RateCalculator.WEIGHT_EXCEPTION);
+        }
+    }
+
+
+
+
+
+
+    @Test
+    public void usa_OTHER_STAMP_OVER50_UpTo100g()
+    {
+        for(int i = 51; i <=100; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.OTHER,
+                    RateCalculator.STAMP, i), 2.95, 0.001);
+        }
+    }
+
+
+    @Test
+    public void usa_OTHER_STAMP_OVER100_UpTo200g()
+    {
+        for(int i = 101; i <=200; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.OTHER,
+                    RateCalculator.STAMP, i), 5.15, 0.001);
+        }
     }
 
     @Test
-    public void canada_OTHER_METER_OR_POSTALINDICIA_Over200_UpTo500g()
+    public void usa_OTHER_STAMP_OVER200_UpTo500g()
     {
-        assertEquals(rate.getRate(RateCalculator.CANADA, RateCalculator.OTHER,
-                RateCalculator.METER_OR_POSTALINDICIA, 403), 9.69, 0.001);
+        for(int i = 201; i <=500; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.OTHER,
+                    RateCalculator.STAMP, i), 10.30, 0.001);
+        }
+    }
+
+    @Test
+    public void usa_OTHER_METER_OR_POSTALINDICIA_Over50_UpTo100g()
+    {
+        for(int i = 51; i <=100; i++)
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.OTHER,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 2.68, 0.001);
+    }
+
+    @Test
+    public void usa_OTHER_METER_OR_POSTALINDICIA_Over100_UpTo200g()
+    {
+        for(int i = 101; i <=200; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.OTHER,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 4.85, 0.001);
+        }
     }
 
 
-
+    @Test
+    public void usa_OTHER_METER_OR_POSTALINDICIA_Over200_UpTo500g()
+    {
+        for(int i = 201; i <=500; i++) {
+            assertEquals(rate.getRate(RateCalculator.USA, RateCalculator.OTHER,
+                    RateCalculator.METER_OR_POSTALINDICIA, i), 9.69, 0.001);
+        }
+    }
 
 }
